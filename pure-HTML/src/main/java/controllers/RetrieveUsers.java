9c705b20b35189e0271f927a0326dfe7cb7378cb
matteo-luraghi <matteoluraghi50@gmail.com>
2024-04-08 13:52:13 +0200

    initialized controllers: created init and destroy methods, still need to install thymeleaf

---
+++ pure-HTML/src/main/java/controllers/RetrieveUsers.java
@@ -0,0 +1,65 @@
+package controllers;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.Connection;
+import utils.ConnectionHandler;
+import javax.servlet.ServletContext;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+/**
+ * Servlet implementation class RetrieveUsers
+ */
+@WebServlet("/RetrieveUsers")
+public class RetrieveUsers extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Connection connection = null;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RetrieveUsers() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    public void init() throws ServletException {
+		connection = ConnectionHandler.getConnection(getServletContext());
+		ServletContext servletContext = getServletContext();
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		this.templateEngine = new TemplateEngine();
+		this.templateEngine.setTemplateResolver(templateResolver);
+		templateResolver.setSuffix(".html");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	
+	public void destroy() {
+		try {
+			ConnectionHandler.closeConnection(connection);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
