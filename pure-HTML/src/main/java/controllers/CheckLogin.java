42fdc0ea184e5e3851b5ee9069774934e8078b68
matteo-luraghi <matteoluraghi50@gmail.com>
2024-04-18 11:40:32 +0200

    added a bit of styling and added valid username memory in login

--- pure-HTML/src/main/java/controllers/CheckLogin.java
+++ pure-HTML/src/main/java/controllers/CheckLogin.java
@@ -3,11 +3,19 @@ package controllers;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
 import utils.ConnectionHandler;
 
 import dao.UserDAO;
@@ -20,13 +28,20 @@ import beans.User;
 public class CheckLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private Connection connection = null;
+	private TemplateEngine templateEngine;
 
     public CheckLogin() {
         super();
     }
 
     public void init() throws ServletException {
-		connection = ConnectionHandler.getConnection(getServletContext());
+		ServletContext servletContext = getServletContext();
+		connection = ConnectionHandler.getConnection(servletContext);
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+ 		templateResolver.setTemplateMode(TemplateMode.HTML);
+		this.templateEngine = new TemplateEngine();
+		this.templateEngine.setTemplateResolver(templateResolver);
+		templateResolver.setSuffix(".html");	
 	}
   
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -34,26 +49,39 @@ public class CheckLogin extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ServletContext servletContext = getServletContext();
+		final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
 		UserDAO uDao = new UserDAO(connection);
 		User u = null;
+		String user_error = "";
 		
 		try {
 			u = uDao.checkCredentials(username, password);
+			if(uDao.checkNewUsername(username)) { // username not in db -> new user
+				user_error = "Username non valido";
+			} else {
+				ctx.setVariable("login_username", username);
+			}
 		} catch (SQLException e) {
 			response.sendError(HttpServletResponse.SC_BAD_GATEWAY, "Failure in database credential checking");
 			return;
 		}
 		
-		String path = getServletContext().getContextPath();
+		String path = servletContext.getContextPath();
 		if (u == null) {
-			path += "/index.html";
+			path = "/index.html";
+			ctx.setVariable("username_login_error", user_error);
+			if (user_error.equals("")) {
+				ctx.setVariable("password_login_error", "Password errata");
+			}
+			templateEngine.process(path, ctx, response.getWriter());
 		} else {
 			request.getSession().setAttribute("user", u);
 			path += "/Homepage";
+			response.sendRedirect(path);
 		}
-		response.sendRedirect(path);
 	}
 
 	public void destroy() {
