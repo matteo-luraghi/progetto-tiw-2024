283845cd7cc6849ec047fa911edffad18300ad0e
matteo-luraghi <matteoluraghi50@gmail.com>
2024-04-27 18:36:41 +0200

    added first js files, css style and Home.html

---
+++ RIA/src/main/webapp/utils.js
@@ -0,0 +1,20 @@
+/**
+ * AJAX call manager 
+ */
+
+ function makeCall(method, url, formElement, cback, reset = true) {
+	 var req = new XMLHttpRequest(); // closure
+	 req.onreadystatechange = function() {
+		 cback(req);
+	 }; // closure
+	 req.open(method, url);
+	 if (formElement == null) {
+		 req.send();
+	 } else {
+		 req.send(new FormData(formElement));
+	 }
+	 
+	 if (formElement != null && reset == true) {
+		 formElement.reset();
+	 }
+ }
